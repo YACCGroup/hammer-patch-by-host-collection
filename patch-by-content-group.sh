7f1ca3bd6487e9eb1647c111dc1ed508484590c0
chris <chris@yaccgroup.com>
2018-04-04 15:10:32 -0600

    update for single hammer call

--- patch-by-content-group.sh
+++ patch-by-content-group.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
 usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
 [ $# -eq 0 ] && usage
 
 # Set default org id
 ORG='MyOrg'
+CSV_ERRATA=''
 
 while getopts "o:c:" arg; do
     case "${arg}" in
@@ -23,14 +23,15 @@ if [ -z $HOST_COLLECTION_NAME ];then
  usage
 fi
 
-hammer --csv host-collection hosts --name $HOST_COLLECTION_NAME --organization $ORG |grep -v '^ID' |cut -f2 -d, |while read HOST
+for HOST in `hammer --csv host-collection hosts --name $HOST_COLLECTION_NAME --organization $ORG |grep -v '^ID' |cut -f2 -d,`
 do
   echo "retrieving $HOST errata"
-  hammer --csv host errata list --host $HOST |grep -v '^ID' |cut -f2 -d, |while read ERRATA
+  for ERRATA in `hammer --csv host errata list --host $HOST |grep -v '^ID' |cut -f2 -d,`
   do
-   #echo "applying errata:$ERRATA"
-   hammer host errata apply --host $HOST --errata-ids $ERRATA
-   #echo "hammer host errata apply --host $HOST --errata-ids $ERRATA"
+   # Build CSV list of errata to apply
+    CSV_ERRATA+="$ERRATA,"
   done
+  # Submit job to apply errata
+  hammer host errata apply --host $HOST --errata-ids "${CSV_ERRATA%?}"
 done
 
